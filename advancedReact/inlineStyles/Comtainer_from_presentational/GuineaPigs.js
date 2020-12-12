506b75fb59be02158fe94f6c23be5bd7e227bcfe
Jonathan <jonathan.george.dev@gmail.com>
2020-12-11 23:46:18 -0800

    container

--- advancedReact/inlineStyles/Comtainer_from_presentational/GuineaPigs.js
+++ advancedReact/inlineStyles/Comtainer_from_presentational/GuineaPigs.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const GUINEAPATHS = [
-  'https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg',
-  'https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg',
-  'https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg',
-  'https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg'
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg",
 ];
 
-export class GuineaPigs extends React.Component {
+class GuineaPigs extends React.Component {
   constructor(props) {
     super(props);
 
@@ -33,9 +33,20 @@ export class GuineaPigs extends React.Component {
     clearInterval(this.interval);
   }
 
+  render() {
+    let src = GUINEAPATHS[this.state.currentGP];
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(<GuineaPigs />, document.getElementById("app"));
 
-  /**
+/**
    * Container Components From Presentational Components
 Separate Container Components From Presentational Components
 
@@ -50,5 +61,37 @@ Instructions
 Click Run. You are looking at a rendered <GuineaPigs /> component.
 
 <GuineaPigs />‘s job is to render a photo carousel of guinea pigs. It does this perfectly well! And yet, it has a problem: it does too much stuff. How might we divide this into a container component and a presentational component?
+
+
+
+
+
+1.
+
+GuineaPigs.js contains a lot of logic! It has to select the correct guinea pig to render, wait for the right amount of time before rendering, render an image, select the next correct guinea pig, and so on.
+
+Let’s separate the logic from the GuineaPigs component into a container component.
+
+Near the top left of the code editor, click on the folder icon. Create a new folder named containers. containers/ should be next to components/.
+
+Inside of containers/, create a new file named GuineaPigsContainer.js. Make sure that GuineaPigs is plural, but Container is singular!
+
+Once you have made containers/GuineaPigsContainer.js, click Run.
+Checkpoint 2 Passed
+2.
+
+Good!
+
+Open components/GuineaPigs.js.
+
+You want to separate the logic of this component class into a container component. How do you do that?
+
+To start, just make a copy. After that, you can delete the appropriate parts.
+
+Highlight the entire contents of components/GuineaPigs.js, and copy it to the clipboard.
+
+Now, open containers/GuineaPigsContainer.js.
+
+Click inside the empty file, and paste. containers/GuineaPigsContainer.js and components/GuineaPigs.js should be identical.
 
    */
